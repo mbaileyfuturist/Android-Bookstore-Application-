4c028b906dcd61a9665e435fb1170b9160c46d98
Han Solo <mbaileyfuturist@gmail.com>
2018-05-22 22:48:13 -0400

    Initial Commit for master branch

---
+++ MainMenu.java
@@ -0,0 +1,25 @@
+package com.example.michealbailey.intrinsicbookstore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void goToPurchaseTransaction(View view){
+        Intent next1 = new Intent(this, PurchaseTransaction.class);
+        startActivity(next1);
+    }
+
+    public void goToInventoryMenu(View view){
+        Intent next2 = new Intent(this, InventoryMenu.class);
+        startActivity(next2);
+    }
+}
